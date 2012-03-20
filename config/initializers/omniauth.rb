5330176fd017b02fa500cd15c87fa6fdbb729c68
designhawg <designhawg@mac.com>
2012-03-20 17:38:45 -0500

    Twitter oAuth enabled

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'Sn04bqObZCSZgHo4tlBEg', '3bkmGuLAmTpT02oZ90aYfPVn0FXwHkwmV10ucB8oAgg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loveapp::Application.config.secret_token = '44c20e33f2e5434f703e9c388461ad18f3271241612b449e35cb5eee590a1950580e118bb81dbfe471760979d6bb0d3224e7a03cb2f76b8ad3a6f91f5411cb39'
